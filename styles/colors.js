a98d49b479ec8188a0b74fbe540f3c125f46cda7
Konolin <mandru.catalin03@gmail.com>
2024-10-30 12:06:22 +0200

    Added the flashcard improvements

--- styles/colors.js
+++ styles/colors.js
@@ -6,4 +6,8 @@ export default {
     buttonBackgroundColor: '#1a1a1a',
     inputBackgroundColor: 'rgb(28,28,30)',
     textColor: 'rgba(255, 255, 255, 0.87)',
+    dieColor: '#FF69B4',
+    verbColor: '#E67E22',
+    adjectiveColor: '#9B59B6',
+    adverbColor: '#E74C3C',
 };
